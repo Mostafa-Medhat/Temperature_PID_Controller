d1bddc01a80eed20cfea1254949ad2dbbb517ce6
Mostafa-Medhat <mmedhat72@gmail.com>
2022-12-12 22:59:40 +0200

    update data client to receive signed data

--- PollingDataClient.c
+++ PollingDataClient.c
@@ -6,10 +6,38 @@
 #include "PollingDataClient.h"
 #include "lcd.h"
 #include "PID.h"
+#include "pwm.h"
 
-void Handle_data(unsigned char data)
+void Handle_data(signed char data)
 {
 	LCD_moveCursor(0, 0);
-	LCD_intgerToString(data);
-//	PIDController_Update(&pid, SET_POINT, data);
+	if(data < 0)
+	{
+		LCD_displayCharacter('-');
+		LCD_intgerToString(-data);
+		if(data>-10)
+		{
+			LCD_displayCharacter(' ');
+		}
+	}
+	else if(data >=0)
+	{
+		LCD_intgerToString(data);
+		if(data<10)
+		{
+			LCD_displayCharacter(' ');
+		}
+		if(data<100)
+		{
+			LCD_displayCharacter(' ');
+		}
+	}
+	float feedbackSignal = PIDController_Update(&pid, SET_POINT, data);
+	if(feedbackSignal < 0)
+	{
+		feedbackSignal = -feedbackSignal;
+	}
+	uint8 duty_cycle= (uint8)((feedbackSignal/100.0)*255);
+	PWM_Timer0_Start(duty_cycle);
+
 }
--- PollingDataClient.h
+++ PollingDataClient.h
@@ -8,7 +8,7 @@
 #ifndef INC_POLLINGDATACLIENT_H_
 #define INC_POLLINGDATACLIENT_H_
 
-void Handle_data(unsigned char data);
+void Handle_data(signed char data);
 
 
 #endif /* INC_POLLINGDATACLIENT_H_ */
